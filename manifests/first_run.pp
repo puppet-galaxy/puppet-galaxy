f53ef3973ae0dab02e85656e1d8fee4ab60f6cc9
Olivier Inizan <oinizan@versailles.inra.fr>
2014-03-18 09:30:35 +0100

    Add stop-daemon in command

--- manifests/first_run.pp
+++ manifests/first_run.pp
@@ -3,7 +3,7 @@ class galaxy::first_run {
      path => '/usr/bin:/usr/sbin:/bin:/sbin',
      cwd  => '/home/galaxy/galaxy-dist',
      user => 'galaxy',
-     command => '/home/galaxy/galaxy-dist/run.sh --daemon',
+     command => '/home/galaxy/galaxy-dist/run.sh --daemon;/home/galaxy/galaxy-dist/run.sh --stop-daemon',
      creates => '/home/galaxy/galaxy-dist/universe_wsgi.ini',
      require => Class['galaxy::update'],
   }
