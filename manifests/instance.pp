cb6b6e5f9d59954fad330dc4c3f9a8398b0f9f61
Eric Rasche <rasche.eric@yandex.ru>
2014-03-24 14:02:47 -0500

    Tool shed conf included and working.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -2,13 +2,19 @@ define galaxy::instance (
   $create_user = $galaxy::params::create_user,
   $directory = $galaxy::params::directory,
   $galaxy_branch = $galaxy::params::galaxy_branch,
+  $custom_toolsheds = [],
 ) {
 
   galaxy::install{ "$name":
     directory => "$directory"
   } -> galaxy::first_run{ "$name":
     directory => "$directory"
-  } -> galaxy::service { "$name": 
-    directory => "$directory"
+  #} -> galaxy::service { "$name": 
+  #  directory => "$directory"
   }
+  
+  galaxy::toolshed_conf{ "$name":
+    directory => "$directory",
+    custom_toolsheds => $custom_toolsheds,
+  } 
 }
