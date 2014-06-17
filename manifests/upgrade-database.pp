7867d4dd0b19f2d003755ee2d83280294e324258
root <root@debian-puppet.local>
2014-06-17 12:33:28 +0000

    First commit.

---
+++ manifests/upgrade-database.pp
@@ -0,0 +1,18 @@
+# == Class galaxy::upgrade-database
+# This class execute manage.db update.
+#
+# === Authors
+# S. Bridel <sebastien.bridel@versailles.inra.fr>
+#
+# === Copyright
+#
+# Copyright 2014, unless otherwise noted.
+class galaxy::upgrade-database() { 
+  $directory = $galaxy::params::directory
+  exec { "galaxy-${name}-eggs-and-universeconf":
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd     => $directory,
+    user    => 'galaxy',
+    command => "sh manage_db.sh upgrade",
+  }
+}
