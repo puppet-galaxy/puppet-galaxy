980cd1d25c304870d4e01059ab08b11a6e38c73d
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-07-08 10:09:38 +0000

    First commit.

---
+++ manifests/common_startup.pp
@@ -0,0 +1,18 @@
+#
+#
+#
+#
+#
+class galaxy::common_startup{
+  include galaxy::params
+  $directory = $galaxy::params::app_directory
+  file { "$directory/common_startup.sh":
+    source  => '/etc/puppet/modules/galaxy/files/common_startup.sh',
+    mode    => 'a=r+w+x',
+  }->
+  exec { 'Common Startup':
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd     => $directory,
+    command => 'sh common_startup.sh',
+  }    
+}
