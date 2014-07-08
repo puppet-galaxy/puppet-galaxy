9c99187fd86e52bf614db3a0da9ca0e109491462
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-07-08 07:48:35 +0000

    Refactoring

--- manifests/toolshed_conf.pp
+++ manifests/toolshed_conf.pp
@@ -25,15 +25,15 @@
 #
 # Copyright 2014, unless otherwise noted.
 #
-define galaxy::toolshed_conf (
-  $directory = $galaxy::params::directory,
+class galaxy::toolshed_conf (
+  $app_directory = $galaxy::params::app_directory,
   $default_toolsheds = [
     { name => 'Galaxy main tool shed', url => 'http://toolshed.g2.bx.psu.edu/' },
     { name => 'Galaxy test tool shed', url => 'http://testtoolshed.g2.bx.psu.edu/' }
   ],
   $custom_toolsheds = [],
 ){
-  file { "$directory/tool_sheds_conf.xml":
+  file { "$app_directory/tool_sheds_conf.xml":
     content => template("galaxy/tool_sheds_conf.xml.erb"),
   }
 
