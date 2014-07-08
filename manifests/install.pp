db5a71fa32e48c36f381ec6b4dc128a5fd196f8b
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-07-08 07:47:35 +0000

    Refactoring

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,7 +10,7 @@
 #
 # [*galaxy_branch*]
 #   Which branch of galaxy's development to pull from. Options are "stable" and
-#   "deafult", where default is the primary branch for development work. 
+#   "deafult", where default is the primary branch for development work.
 #
 # === Examples
 #
@@ -27,21 +27,21 @@
 #
 # Copyright 2014, unless otherwise noted.
 #
-define galaxy::install(
-  $directory = $galaxy::params::directory,
-  $galaxy_branch = $galaxy::params::galaxy_branch,
+class galaxy::install(
+  $install_directory = $galaxy::params::app_directory,
+  $galaxy_branch  = $galaxy::params::galaxy_branch,
 ){
 
   # There is a "bug" here in that every puppet run we'll get notifications of switching branches, even though we're not actually making changes. E.g.,
   #
   #Notice: /Stage[main]/Custom::Puppetlxc/Galaxy::Instance[test]/Galaxy::Install[test]/Vcsrepo[/home/galaxy/galaxy-dist/]/revision: revision changed '29ce93a13ac7' to 'default'
-  # 
+  #
   # I'm not sure what the best course of action here is. Let the module clutter our logs up or switch to running commands (test for branch/run with onlyif)
-  vcsrepo { $directory:
+  vcsrepo { $install_directory:
     ensure => present,
     user => 'galaxy',
     provider => hg,
     source => 'https://bitbucket.org/galaxy/galaxy-dist/',
     revision => $galaxy_branch,
-  } 
+  }
 }
