18374a447f78d362bf4ae3976f0742eb24f8d23d
Sébastien Bridel <sebastien.bridel@etu.upmc.fr>
2014-07-10 09:29:10 +0000

    Add require

--- manifests/service.pp
+++ manifests/service.pp
@@ -20,21 +20,24 @@
 #
 class galaxy::service (
   $directory = $galaxy::params::app_directory
-){
+) 
+inherits galaxy::params 
+{   
   case $osfamily {
-    Debian: { $source='galaxy/galaxy.debian-service.erb'}
+    Debian: { $source='galaxy/galaxy-debian-service.erb'}
     RedHat: { $source='galaxy/galaxy.fedora-init.erb'}
     default: {fail('no init script for this osfamily')}
   }
-  file { "/etc/init.d/galaxy-$name":
-    content  => template($source),
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0755',
+  file { '/etc/init.d/galaxy-service':
+    content => template($source),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['galaxy::universe']
   }-> 
-  service { "galaxy-$name" :
-    ensure     => 'running',
-    enable     => true,
-    require    => File["/etc/init.d/galaxy-$name"],
+  service { 'galaxy-service' :
+    ensure  => 'running',
+    enable  => true,
+    require => File['/etc/init.d/galaxy-service'],
   }
 }
