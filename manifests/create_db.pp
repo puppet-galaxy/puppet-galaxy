acb83aef8855a9626db4c59443f443820a1d3362
sbridel <sebastien.bridel@versailles.inra.fr>
2014-07-11 14:11:27 +0000

    First Commit.

---
+++ manifests/create_db.pp
@@ -0,0 +1,17 @@
+#
+#
+#
+#
+#
+#
+class galaxy::create_db {
+  $app_directory = $galaxy::params::app_directory
+  exec { 'create_db.sh':
+    require =>  Class ['galaxy::universe'],
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd     => $app_directory,
+    user    => 'galaxy',
+    command => 'sh create_db.sh',
+    timeout => 0,
+  }
+}
