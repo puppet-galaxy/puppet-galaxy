e6bd1bc4374b7f7792c14d4831c98354c57f48e3
oinizan <oinizan@debian>
2014-07-17 16:02:24 +0200

    file section: modify require,add owner 'galaxy' -exec section:  add user 'galaxy', add require

--- manifests/common_startup.pp
+++ manifests/common_startup.pp
@@ -7,13 +7,16 @@ class galaxy::common_startup{
   include galaxy::params
   $directory = $galaxy::params::app_directory
   file { "$directory/common_startup.sh":
-    require => Class['galaxy::install'],
+    require => Class['galaxy::job_conf'],
     source  => '/etc/puppet/modules/galaxy/files/common_startup.sh',
     mode    => 'a=r+w+x',
+    owner   => 'galaxy'
   }->
   exec { 'Common Startup':
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     cwd     => $directory,
     command => 'sh common_startup.sh',
+    user    => 'galaxy',
+    require => File["$directory/common_startup.sh"],
   }
 }
