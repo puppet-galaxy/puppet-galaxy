80eb2159b75d0baabbd1c64919fc17be388c97be
oinizan <oinizan@debian>
2014-07-17 16:05:33 +0200

    Modify require

--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -7,7 +7,7 @@
 class galaxy::create_db {
   $app_directory = $galaxy::params::app_directory
   exec { 'create_db.sh':
-    require =>  Class ['galaxy::universe'],
+    require =>  Class ['galaxy::common_startup'],
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     cwd     => $app_directory,
     user    => 'galaxy',
