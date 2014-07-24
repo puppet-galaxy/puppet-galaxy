b631793919712a25c7023deb76f067fbfb944e73
sbridel <sebastien.bridel@versailles.inra.fr>
2014-07-24 09:07:16 +0000

    Run as executable without use of sh.

--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -5,14 +5,14 @@
 #
 #
 class galaxy::create_db {
-  $app_directory = $galaxy::params::app_directory
+  $directory = $galaxy::params::app_directory
   exec { 'create_db.sh':
     require =>  Class ['galaxy::common_startup'],
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-    cwd     => $app_directory,
+    cwd     => $directory,
     user    => 'galaxy',
     group   => 'galaxy',
-    command => 'sh create_db.sh',
+    command => "$directory/create_db.sh",
     timeout => 500,
   }
 }
