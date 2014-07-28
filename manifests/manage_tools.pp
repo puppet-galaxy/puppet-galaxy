3ba4d36558a97b9c3bd56a442e0a5ac969193409
sbridel <sebastien.bridel@versailles.inra.fr>
2014-07-28 09:50:35 +0000

    Require create_db.pp

--- manifests/manage_tools.pp
+++ manifests/manage_tools.pp
@@ -5,12 +5,12 @@
 #
 class galaxy::manage_tools {
   $directory = $galaxy::params::app_directory
-  exec { 'Common Startup':
-  path =>'/usr/bin:/usr/sbin:/bin:/sbin',
-  cwd => $directory,
-  command => "python $directory/manage_tools.py upgrade",
-  user => 'galaxy'
-  group => 'galaxy',
-  require => Class['galaxy::create_db']
+  exec { 'Manage Tools Upgrade':
+  path    =>'/usr/bin:/usr/sbin:/bin:/sbin',
+  cwd     => $directory,
+  command => "python $directory/scripts/manage_tools.py upgrade",
+  user    => 'galaxy',
+  group   => 'galaxy',
+  require  => Class['galaxy::create_db'],
   }
 }
